9f55bd2ef17e5fcad0695a29b731d5c3645e65f5
xly <leyang.xue@ed.ac.uk>
2024-09-05 18:56:16 +0100

    add param meta

--- tests/python/test_param_offload.py
+++ tests/python/test_param_offload.py
@@ -8,7 +8,7 @@ model_name = "facebook/opt-125m"
 config = AutoConfig.from_pretrained(model_name)
 
 engine = EmulationEngine(config)
-engine_config = EmulatorConfig(gpu_memory=0.5, cpu_memory=0.5, ckpt_path="checkpoints")
+engine_config = EmulatorConfig(gpu_memory=0.5, cpu_memory=0.5, ckpt_path="../checkpoints")
 
 with engine.init(OPTForCausalLM, engine_config):
     model = OPTForCausalLM.from_pretrained(model_name)
