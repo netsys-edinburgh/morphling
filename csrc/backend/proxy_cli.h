f59f2a3c4f409105f95e21c36523effb5de53eae
Jiaxun Yang <jiaxun.yang@ed.ac.uk>
2026-01-09 17:30:18 +0000

    proxy_cli: Rework to use cuBlasXT
    Signed-off-by: Jiaxun Yang <jiaxun.yang@ed.ac.uk>

--- csrc/backend/proxy_cli.h
+++ csrc/backend/proxy_cli.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <cublas_v2.h>
-
 #include "common/env_cfg.h"
 #include "common/lru.h"
 #include "common/pytorch_defs.h"
@@ -17,7 +15,6 @@ namespace backend {
 class ProxyCliHandle : public uevent::LoopHandle {
  public:
   ProxyCliHandle(ProxyEnvCfg& ctx, uevent::UeventLoop* loop);
-  ~ProxyCliHandle();
 
   static void ThreadInit(uevent::UeventLoop* loop);
   static uevent::LoopHandle* CreateMyself(ProxyEnvCfg& ctx,
@@ -36,13 +33,15 @@ class ProxyCliHandle : public uevent::LoopHandle {
                        const MatrixPartition& partition);
 
  private:
-  // cuBLAS helper methods
-  void InitCublas();
-  void CleanupCublas();
-
   ProxyEnvCfg& ctx_;
   uevent::UeventLoop* loop_;
-  cublasHandle_t cublas_handle_;
+};
+
+struct CachedTensor {
+  void* data = nullptr;
+  int64_t rows = 0;
+  int64_t cols = 0;
+  int64_t bytes = 0;
 };
 
 class ProxyCliImpl : public std::enable_shared_from_this<ProxyCliImpl> {
@@ -90,7 +89,7 @@ class ProxyCliImpl : public std::enable_shared_from_this<ProxyCliImpl> {
   std::string uuid_;
   std::vector<MatrixPartition> cached_partitions_;
   std::unordered_set<PtrData> cached_msgs_;
-  FixSizeLRUCache<TensorKey, torch::Tensor> cached_tensors_;
+  FixSizeLRUCache<TensorKey, CachedTensor> cached_tensors_;
 };
 
 typedef std::shared_ptr<ProxyCliImpl> ProxyCliImplPtr;
