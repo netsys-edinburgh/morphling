54c5846d81f4459d251ed25a2686162cb941961e
xly <leyang.xue@ed.ac.uk>
2026-04-05 13:59:02 +0100

    fix: resolve FIXME technical debt in autograd, model_emulator, and run_device

--- morphling/runtime/model_emulator.py
+++ morphling/runtime/model_emulator.py
@@ -37,7 +37,25 @@ from tqdm import tqdm
 from transformers import AutoConfig, PreTrainedModel
 from transformers.configuration_utils import PretrainedConfig
 
-from morphling._C import ArcherTensorHandle, MemoryManagerClient, set_tensor_shm
+try:
+    from morphling._C import (
+        ArcherTensorHandle,
+        MemoryManagerClient,
+        set_tensor_shm,
+    )
+except ImportError:
+    from morphling._C import set_tensor_shm
+
+    class _MissingNativeBackend:
+        def __init__(self, *args, **kwargs):
+            raise ImportError(
+                "morphling._C is missing ArcherTensorHandle/"
+                "MemoryManagerClient symbols"
+            )
+
+    ArcherTensorHandle = _MissingNativeBackend
+    MemoryManagerClient = _MissingNativeBackend
+
 
 # from morphling._intercept import MemoryManagerClient
 from morphling.common import EmulatorConfig
@@ -304,10 +322,12 @@ class EmulationEngine(object):
             return do_nothing
 
         def post_init_decorator(orig_post_init: Callable) -> Callable:
-            # FIXME: this is a hacky way to get rid of the write to weight in the post_init, need a better way to do this if we need to support model training
             @functools.wraps(orig_post_init)
             def archer_post_init(cls, *args, **kwargs):
-                pass
+                from transformers.modeling_utils import no_init_weights
+
+                with no_init_weights():
+                    return orig_post_init(cls, *args, **kwargs)
 
             return archer_post_init
 
